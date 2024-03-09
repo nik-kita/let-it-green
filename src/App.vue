92a2fe90c7e256cab8ea6f8150817f16f8befd4b
nik-kita <nik-kita.deno.dev>
2024-03-09 23:37:35 +0100

    Button

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
+import Button from './components/Button.vue';
 </script>
 
 <template>
+  <div class="app root">
+    <Button :sticky="['top', 'left']">Click me!</Button>
+  </div>
 </template>
+
+<style>
+.app.root {
+  height: 100%;
+  background-color: greenyellow;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { computed, withDefaults } from 'vue';
+
+const props = withDefaults(defineProps<{
+  sticky?: ('left' | 'right' | 'top' | 'bottom')[];
+  disabled?: boolean;
+}>(), {});
+const clazz = computed(() => {
+  return [
+    props.sticky?.map((position) => `sticky-${position}`),
+  ];
+});
+</script>
+<template>
+  <button class="btn" :class="clazz">
+    <slot></slot>
+  </button>
+</template>
+
+<style>
+.btn {
+  cursor: pointer;
+}
+
+.btn.sticky-top {
+  position: sticky;
+  top: 1rem;
+}
+
+.btn.sticky-bottom {
+  position: sticky;
+  bottom: 1rem;
+}
+
+.btn.sticky-left {
+  position: sticky;
+  left: 1rem;
+}
+
+.btn.sticky-right {
+  position: sticky;
+  right: 1rem;
+}
+</style>
