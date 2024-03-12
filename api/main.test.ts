2e8e6fee97f35a683855dbfd82f93adc9409d2de
nik-kita <nik-kita.deno.dev>
2024-03-12 02:38:28 +0100

    typescript generics for SlottableStuff

--- api/main.test.ts
+++ api/main.test.ts
@@ -1,5 +1,49 @@
 import { assertEquals } from "https://deno.land/std@0.219.0/assert/mod.ts";
 
+class SlottableStuff<T extends [string, any][], U extends [string, any][] = T> {
+  private constructor() {}
+  public static init<
+    T extends [string, any][] = [],
+    U extends [string, any][] = T,
+  >() {
+    return new SlottableStuff<T, U>();
+  }
+  add_new<S extends string, V extends any>(
+    str: S extends T[number][0] ? never : S,
+    value: V,
+  ) {
+    return this as unknown as (Exclude<[...T, [S, V]][number], 1> extends
+      Exclude<[...U, [S, V]][number], 1> ? SlottableStuff<
+        [...T, [S, V]],
+        [...U, [S, V]]
+      >
+      : Omit<
+        SlottableStuff<
+          [...T, [S, V]],
+          [...U, [S, V]]
+        >,
+        "do"
+      >);
+  }
+  add<S extends T[number][0], V extends any>(str: S, value: V) {
+    return this as unknown as (Exclude<T[number], 1> extends
+      Exclude<[...U, [S, V]][number], 1> ? SlottableStuff<T, [...U, [S, V]]>
+      : Omit<SlottableStuff<T, [...U, [S, V]]>, "do">);
+  }
+  do() {
+  }
+}
+
+Deno.test("SlottableStuff", () => {
+  const stuff = SlottableStuff.init().add_new("a", 1).add_new(
+    "b",
+    2,
+  ).add(
+    "b",
+    3,
+  ).add("a", 4).add_new("c", 6);
+});
+
 function make_slottable<
   Origin extends (...arg: any[]) => any,
 >(
